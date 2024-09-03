e44bf70ad3476d9ff99393c89c8c712b364aae75
MarcoM3j14 <marcomejia2001@gmail.com>
2024-09-03 15:02:05 -0600

    Creación de ruta Create books

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -5,5 +5,6 @@ const router = Router();
 const bookController = new BookController();
 
 router.get("/books", bookController.getAll);
+router.post('/books', bookController.create);
 
 export default router;
