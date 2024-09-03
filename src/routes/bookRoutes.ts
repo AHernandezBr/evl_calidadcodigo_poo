5c1e23b9e204ec10f1ce98a7a5553f417b197657
AHernandezBr <alejandro.hernandezb@axity.com>
2024-09-03 15:36:48 -0600

    comentar rutas

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -6,13 +6,13 @@ const bookController = new BookController();
 
 router.get("/books", bookController.getAll);
 router.post('/books', bookController.create);
-router.get('/books/:id', bookController.getById);
-router.put('/books/:id', bookController.update);
-router.delete('/books/:id', bookController.delete);
-router.post('/editorials', editorialController.create);
-router.get('/editorials', editorialController.getAll);
-router.get('/editorials/:id', editorialController.getById);
-router.put('/editorials/:id', editorialController.update);
-router.delete('/editorials/:id', editorialController.delete);
+// router.get('/books/:id', bookController.getById);
+// router.put('/books/:id', bookController.update);
+// router.delete('/books/:id', bookController.delete);
+// router.post('/editorials', editorialController.create);
+// router.get('/editorials', editorialController.getAll);
+// router.get('/editorials/:id', editorialController.getById);
+// router.put('/editorials/:id', editorialController.update);
+// router.delete('/editorials/:id', editorialController.delete);
 
 export default router;
