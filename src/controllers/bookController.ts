a5971f7043eb785b284f79e96d160279a9fe4071
MarcoM3j14 <marcomejia2001@gmail.com>
2024-09-03 16:34:45 -0600

    Creacion de create

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -16,15 +16,26 @@ export class BookController {
     }
   };
 
+  // create = async (req: Request, res: Response) => {
+  //   let book = new Book(3, "The Silmarillion", "J.R.R. Tolkien", 1977, 1);
+  //   try {
+  //     const newBook = await this.bookService.createBook(book);
+  //     res.status(200).json(newBook);
+  //   } catch (error) {
+  //     res.status(500).json({ error: "Error creating book" });
+  //   }
+  // };
+
   create = async (req: Request, res: Response) => {
-    let book = new Book(3, "The Silmarillion", "J.R.R. Tolkien", 1977, 1);
+    const { title, author, year, editorialId } = req.body;
+    const newBook = new Book(1, title, author, year, editorialId); // ID será generado automáticamente por la base de datos
     try {
-      const newBook = await this.bookService.createBook(book);
-      res.status(200).json(newBook);
+      const createdBook = await this.bookService.createBook(newBook);
+      res.status(201).json(createdBook);
     } catch (error) {
       res.status(500).json({ error: "Error creating book" });
     }
-
-
   };
+
+
 }
--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -13,9 +13,15 @@ export class BookService implements IbookService{
   getBookById(id: number): Promise<Book> {
     throw new Error("Method not implemented.");
   }
-  createBook(book: Book): Promise<Book> {
-    throw new Error("Method not implemented.");   
+  async createBook(book: Book): Promise<Book> {
+    const result = await pool.query(
+      'INSERT INTO books (id, title, author, year, editorialid) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [book.id, book.title, book.author, book.year, book.editorialId]
+    );
+    const row = result.rows[0];
+    return new Book(row.id, row.title, row.author, row.year, row.editorialid);
   }
+
   updateBook(book: Book): Promise<Book> {
     throw new Error("Method not implemented.");
   }
